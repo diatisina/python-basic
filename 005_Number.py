836bde6e6fe6281092257ab5675f7a728e497f7b
Supardi Atisina <163962403+diatisina@users.noreply.github.com>
2025-04-08 23:33:48 +0700

    Add files via upload
    Python Basic

---
+++ 005_Number.py
@@ -0,0 +1,23 @@
+a = 10
+b = 50.5
+c = 2j
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+d = float(a)
+e = int(b)
+f = complex(a)
+
+print(d)
+print(e)
+print(f)
+print(type(d))
+print(type(e))
+print(type(f))
+
+import random
+print(random.randrange(1,100))
